a8eaad5c99b3e21512d32043f75ad0633c575295
NesQ1k <111gosha11@gmail.com>
2018-04-24 21:47:57 +0400

    БД

---
+++ Drocsid/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drocsid.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private Models.DrocsidEntities db = new Models.DrocsidEntities();
+
+        public ActionResult Index()
+        {
+            var Items = db.Users;
+            return View();
+        }
+
+        public ActionResult ShowUser()
+        {
+            var Items = db.Users;
+            string result = "";
+            foreach(var i in Items)
+            {
+                result += "<li>Name = " + i.username + "<li>";
+            }
+            return Content(result);
+        }
+    }
+}
