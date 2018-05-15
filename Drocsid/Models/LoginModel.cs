c312eaf971b6e4b9f9ce2915108f28326b86eb1b
sabbkull <knubov@bk.ru>
2018-05-15 20:48:51 +0400

    html+c# design~
    Переделал обычную на c# верстку

--- Drocsid/Models/LoginModel.cs
+++ Drocsid/Models/LoginModel.cs
@@ -9,9 +9,15 @@ namespace Drocsid.Models
     public class LoginModel
     {
         [Required]
+        [Display(Name = "Логин")]
         public string Username { get; set; }
+
         [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
     }
 }
--- Drocsid/Models/SignupModel.cs
+++ Drocsid/Models/SignupModel.cs
@@ -9,12 +9,24 @@ namespace Drocsid.Models
     public class SignupModel
     {
         [Required]
+        [EmailAddress]
+        [Display(Name = "Почта")]
         public string Email { get; set; }
+
         [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 3)]
+        [Display(Name = "Логин")]
         public string Username { get; set; }
+
         [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
-        [Required]
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Повтор пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
         public string ConfrimPassword { get; set; }
        /* [Required]
         public string Avatar { get; set; }*/
